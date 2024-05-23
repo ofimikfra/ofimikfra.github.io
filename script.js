d2724be2e5858ea20fa012ac925456f613468635
naourk <sofiamikaelafrancisco@gmail.com>
2024-05-24 01:00:07 +0400

    added project, certification & contact page

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function yay(id) {
-    id.textContent += " !"
+    id.textContent += "!"
 }
 
 function bye(id) {
-    id.textContent = id.textContent.slice(0, -2);
+    id.textContent = id.textContent.slice(0, -1);
+}
+
+function validateForm() {
+    let email = document.forms["contact"]["email"].value;
+    if (!email.match(/\w\S+@\S+(\.\S+){1,}/)) {
+        document.getElementById("error").textContent = "Please enter a valid email address."
+        return false
+    }
+
+    errorElement.textContent = "";
+    return true;
 }
