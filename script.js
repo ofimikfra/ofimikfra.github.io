3f4a0dad7c01098306c630f2ad7ada264b75eefe
naourk <sofiamikaelafrancisco@gmail.com>
2024-05-26 21:01:31 +0400

    made layout for project page, added animations

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ function bye(id) {
     id.textContent = id.textContent.slice(0, -1);
 }
 
+function goproj(id) {
+    const h2 = id.parentElement;
+    h2.classList.add('hovered');
+}
+
+function byeproj(id) {
+    const h2 = id.parentElement;
+    h2.classList.remove('hovered');
+}
+
 function validateForm() {
     let email = document.forms["contact"]["email"].value;
     if (!email.match(/\w\S+@\S+(\.\S+){1,}/)) {
