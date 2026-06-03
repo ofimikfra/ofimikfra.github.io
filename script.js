0e8502af0ae86859ddf80d21b0987876ec9891af
naourk <sofiamikaelafrancisco@gmail.com>
2026-06-03 17:35:14 +0400

    UI revamp

--- script.js
+++ script.js
@@ -1,36 +1,101 @@
-function yay(id) {
-    id.textContent += "!"
+const navIcons = document.querySelectorAll(".navIcon");
+const contactIcons = document.querySelectorAll(".contactIcon");
+const navShadow = document.getElementById("navShadow");
+const sideBar = document.querySelector(".sideBar");
+const svgs = sideBar.querySelectorAll("svg");
+
+const icons = [...navIcons, ...contactIcons]
+
+
+function hoverOnEach(elements) {
+    elements.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            element.classList.add("hover");
+        });
+        element.addEventListener('mouseleave', () => {
+            element.classList.remove("hover");
+        });
+    });
 }
 
-function bye(id) {
-    id.textContent = id.textContent.slice(0, -1);
-}
+hoverOnEach(svgs);
 
-function goproj(id) {
-    const h2 = id.parentElement;
-    h2.classList.add('hovered');
-}
+icons.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        const label = element.querySelector(".label")
+        label.classList.add("hover");
+        element.classList.add("hover");
+    });
+    element.addEventListener('mouseleave', () => {
+        const label = element.querySelector(".label")
+        label.classList.remove("hover");
+        element.classList.remove("hover");
+    });
+});
 
-function byeproj(id) {
-    const h2 = id.parentElement;
-    h2.classList.remove('hovered');
-}
+sideBar.addEventListener('mouseenter', () => {
+    navShadow.classList.add("hover");
+});
 
-function toggleNav() {
-    var nav = document.querySelector('.nav');
-    var main = document.querySelector('.main');
-    nav.classList.toggle('active');
-    main.classList.toggle('blurred');
-}
+sideBar.addEventListener('mouseleave', () => {
+    navShadow.classList.remove("hover");
+});
 
-function handleScroll() {
-    var profile = document.querySelector('.profile');
-    if (window.scrollY === 0) {
-        profile.classList.remove('scrolled');
+/* ---------------------------- typing animation ---------------------------- */
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1500;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-        profile.classList.add('scrolled');
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-}
 
-window.addEventListener('scroll', handleScroll);
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
+    var that = this;
+    var delta = 150 - Math.random() * 40;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 300;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap::after { content: ':'; display: inline-block; margin-left: 0.1em; color: #fff; }";
+    document.body.appendChild(css);
+};
